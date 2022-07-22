90a961861d2008f63f6454bf693beaa7971cccc7
Wolfgang Szwillus <wolfgang.szwillus@ifg.uni-kiel.de>
2022-07-22 11:37:12 +0200

    feat: Update to python3

--- func_dump.py
+++ func_dump.py
@@ -1,11 +1,8 @@
-"""Miscallenous functions used for the kriging interpolation
-"""
-
 import numpy as np
+from scipy.special import kv,gamma
 
 def get_pairwise_geo_distance(lon,lat):
-    """Calculate great-circle distance between all pairs of points
-    """
+    # Calculate spherical distance
     N = len(lon)
     pd = np.zeros((N,N))
     coslat = np.cos(lat/180.0*np.pi)
@@ -21,8 +18,6 @@ def get_pairwise_geo_distance(lon,lat):
     return pd
 
 def get_pairwise_cross_distance(lon1,lat1,lon2,lat2):
-    """Calculate great-circle distance between two lists of points
-    """
     N1 = len(lon1)
     N2 = len(lon2)
     
@@ -42,29 +37,17 @@ def get_pairwise_cross_distance(lon1,lat1,lon2,lat2):
     return pd
     
 def get_all_geo_distance(lon,lat,lon0,lat0):
-    """Calculate great-circle distance between one point and a list of points
-    """
+    N = len(lon)
     coslat = np.cos(lat/180.0*np.pi)
     sinlat = np.sin(lat/180.0*np.pi)
     
     coslat0 = np.cos(lat0/180.0*np.pi)
     sinlat0 = np.sin(lat0/180.0*np.pi)
-    
+        
+
     dx = lon - lon0
     cosdx = np.cos(dx/180.0*np.pi)
     pd = coslat0*coslat*cosdx + sinlat0*sinlat
     pd[pd>1] = 1
     pd = 180.0/np.pi*np.arccos(pd)
     return pd
-
-def C_sph_nugget(d,covarParams):
-    """Evaluate spherical covariance function
-    """
-    C = np.zeros(d.shape)
-    if covarParams[2] == 0.0:
-        np.fill_diagonal(C,covarParams[0] + covarParams[1])
-    else:
-        C[d==0] = covarParams[0] + covarParams[1]
-        C[(d>0)&(d<covarParams[2])] =  covarParams[1] * (1 - 1.5*d[(d>0)&(d<covarParams[2])]/covarParams[2] + 0.5 * d[(d>0)&(d<covarParams[2])]**3  / covarParams[2]**3)
-        C[(d>0)&(d>=covarParams[2])] =  0.0
-    return C
